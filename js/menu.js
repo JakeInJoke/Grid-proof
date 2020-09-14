198e63c07f7b3784bf0180443b09e93c6a92c07f
Blurry-Jake <htx4569@gmail.com>
2020-09-14 17:20:31 -0500

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,183 @@
+import { loadOn, chartJs } from "/js/myQuery.js";
+const title = document.getElementById("tit");
+
+const _ROUTE = "templates/";
+
+var menuItem = document.querySelectorAll(".menu-item");
+
+//carga inicial
+firstLoad();
+
+/*recorre de manera inicial todos los items del menu generando una 
+funcion personalizada para cada uno de ellos*/
+for (var i = 0; i < menuItem.length; i++) {
+  menuItem[i].onclick = function (e) {
+    var id = e.target.id;
+    if (id) {
+      id = id;
+    } else {
+      id = e.target.offsetParent.id;
+    }
+    var isActive = getId(id).classList.contains("active");
+    if (!isActive) {
+      removeClFromAll(menuItem, "active");
+      getId(id).classList.add("active");
+    }
+    loadTo(id, _ROUTE + id + ".html", "#rq", "main");
+    funcionOfFunctions(id);
+  };
+}
+
+//funcion para cargar informacion de una pagina en otra
+function loadTo(id, url, from, where) {
+  var fromTemplate = new loadOn(url, from, where, true);
+  fromTemplate.load();
+  title.innerHTML = getId(id).innerHTML.toString();
+}
+//funcion de primera carga de la aplicacion
+function firstLoad() {
+  loadTo("tit", "templates/resumen.html", "#rq", "main");
+  title.innerHTML = getId("resumen").innerHTML.toString();
+  funcionOfFunctions("resumen");
+}
+
+function funcionOfFunctions(id) {
+  switch (id) {
+    case "resumen":
+      optiChars(1);
+      optiChars(2);
+      optiChars(3);
+      break;
+
+    default:
+      break;
+  }
+}
+function optiChars(page) {
+  switch (page) {
+    case 1:
+      setTimeout(() => {
+        //createChart(id, type, data, beginAtZero, legend, verX, verY)
+        createChart(
+          "chartA", //lugar donde se hospeda el grafico
+          "bar", //la forma que tiene el grafico
+          "Data de los colores",
+          "/json/data.json", //el lugar donde están los datos que se ve en el grafico
+          true, //si quieres que comience en Cero el grafico
+          true, //Si quieres que se vean las etiquetas o leyenda
+          true, // Si quieres que no se ve el eje x
+          true // Si quieres que no se vea el eje Y
+        );
+      }, 100); // 200 milisegundos
+      break;
+    case 2:
+      setTimeout(() => {
+        //createChart(id, type, data, beginAtZero, legend, verX, verY)
+        createChart(
+          "chartB", //lugar donde se hospeda el grafico
+          "pie", //la forma que tiene el grafico
+          "Data de los colores",
+          "/json/data.json", //el lugar donde están los datos que se ve en el grafico
+          true, //si quieres que comience en Cero el grafico
+          true, //Si quieres que se vean las etiquetas o leyenda
+          false, // Si quieres que no se ve el eje x
+          false // Si quieres que no se vea el eje Y
+        );
+      }, 100); // 200 milisegundos
+      break;
+    case 3:
+      setTimeout(() => {
+        //createChart(id, type, data, beginAtZero, legend, verX, verY)
+        createChart(
+          "chartC", //lugar donde se hospeda el grafico
+          "radar", //la forma que tiene el grafico
+          "Data de los colores",
+          "/json/data.json", //el lugar donde están los datos que se ve en el grafico
+          true, //si quieres que comience en Cero el grafico
+          true, //Si quieres que se vean las etiquetas o leyenda
+          false, // Si quieres que no se ve el eje x
+          false // Si quieres que no se vea el eje Y
+        );
+      }, 100); // 200 milisegundos
+      break;
+
+    default:
+      break;
+  }
+}
+
+const burguer = getId("burger");
+const closeburguer = getId("close-burger");
+var nav_burguer_btn = [burguer, closeburguer];
+var darkDiv = getId("darker");
+var darkB = false;
+nav_burguer_btn.forEach((button) => {
+  button.onclick = function (e) {
+    var sidebar = getId("sidebar");
+    toggleClass(darkDiv, "view-dr");
+    toggleClass(sidebar, "visible-nb");
+    toggleClass(burguer, "no-visible");
+    toggleClass(closeburguer, "visible-btn");
+  };
+});
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//funcion que reemplaza al getelementbyid
+function getId(id) {
+  var id = document.getElementById(id);
+  return id;
+}
+//funcion que reemplaza al getelementsbyclassname
+function getClass(cla) {
+  var cla = document.getElementsByClassName(cla);
+  return cla;
+}
+//agregar a la lista de clases
+function addClass(id, clase) {
+  id.classList.add(clase);
+}
+//remover clases o clase
+function removeClass(id, clase) {
+  id.classList.remove(clase);
+}
+//cambiar clases por completo
+function changeClass(id, clase) {
+  id.className = clase;
+}
+//toggle class
+function toggleClass(id, clase) {
+  id.classList.toggle(clase);
+}
+//fucnion que remueve cierta clase de un grupo de items
+function removeClFromAll(items, torem) {
+  [].forEach.call(items, function (el) {
+    el.classList.remove(torem);
+  });
+}
+//funcion para dibujar los graficos estadisticos
+function createChart(id, type, label, data, beginAtZero, legend, verX, verY) {
+  var chart = new chartJs(
+    id,
+    type,
+    label,
+    data,
+    beginAtZero,
+    legend,
+    verX,
+    verY
+  );
+  chart.create();
+}
---
+++ js/myQuery.js
@@ -0,0 +1,198 @@
+class loadOn {
+  constructor(url, sourceContainer, targetContainer, replace) {
+    this.url = url;
+    this.sourceContainer = sourceContainer;
+    this.targetContainer = targetContainer;
+    this.replace = replace;
+  }
+
+  load() {
+    var xhr = new XMLHttpRequest();
+    var url = this.url;
+    var source = this.sourceContainer;
+    var target = this.targetContainer;
+    var replace = this.replace;
+    xhr.onerror = function () {
+      throw "Request failed. HTTP code " + xhr.status;
+    };
+    xhr.onload = function () {
+      if (!xhr.status || xhr.status >= 400) {
+        throw "Request failed. HTTP code " + xhr.status;
+      }
+      var temp = document.createElement("div");
+      temp.innerHTML = xhr.responseText;
+      var ele = temp.querySelector(source);
+      //console.log(temp);
+
+      if (ele) {
+        if (replace) {
+          document.getElementById(target).innerHTML = ele.outerHTML;
+        } else {
+          document.getElementById(target).appendChild(ele);
+        }
+      }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
+  }
+}
+
+class dinamicCss {
+  constructor(id, href) {
+    this.id = id;
+    this.href = href;
+  }
+  create() {
+    var id = this.id;
+    var href = this.href;
+
+    if (!document.getElementById(id)) {
+      var head = document.getElementsByTagName("head")[0];
+      var link = document.createElement("link");
+      link.id = id;
+      link.rel = "stylesheet";
+      link.type = "text/css";
+      link.href = href;
+      link.media = "all";
+      head.appendChild(link);
+    }
+  }
+}
+/*class dinamicJs {
+  constructor(id, type, src) {
+    this.id = id;
+    this.type = type;
+    this.src = src;
+  }
+  create() {
+    var id = this.id;
+    if (!document.getElementById(id)) {
+      this.build();
+    } else {
+      this.destroy();
+      setTimeout(() => {
+        this.build();
+      }, 500);
+    }
+  }
+  build() {
+    var id = this.id;
+    var type = this.type;
+    var src = this.src;
+
+    var body = document.getElementsByTagName("body")[0];
+    var script = document.createElement("script");
+    script.id = id;
+    script.type = type;
+    script.src = src;
+    body.appendChild(script);
+  }
+  destroy() {
+    var id = this.id;
+    var el = document.getElementById(id);
+    el.parentNode.removeChild(el);
+  }
+}*/
+
+class chartJs {
+  constructor(id, type, label, data, beginAtZero, legend, verX, verY) {
+    this.id = id; //string
+    this.type = type; //string
+    this.label = label;
+    this.data = data; //string json
+    this.beginAtZero = beginAtZero; //boolean
+    this.legend = legend; //boolean
+    this.verX = verX;
+    this.verY = verY;
+  }
+  create() {
+    //Parámetros del objeto
+    var id = this.id;
+    var type = this.type;
+    var label = this.label;
+    var data = this.data;
+    var beginAtZero = this.beginAtZero;
+    var legend = this.legend;
+    var verX = this.verX;
+    var verY = this.verY;
+
+    //Variables extra
+    var letter = "";
+    var arrayData1 = [];
+    var arrayData2 = [];
+
+    //HTTPREQ PARA OBTENER EL ARCHIVO DE DATOS
+    var xml = new XMLHttpRequest();
+    xml.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        //response text es un texto de respuesta que consigue cuando hay datos
+        //que recolectar de la ruta indicada
+        var myObj = JSON.parse(this.responseText);
+        letter = myObj.colors;
+        //console.log(letter);
+      }
+    };
+    xml.open("GET", data, true);
+    xml.send();
+    //FIN DEL HTTPREQ
+
+    setTimeout(() => {
+      function getData() {
+        letter.forEach((color) => {
+          arrayData1.push(color["color"]);
+          arrayData2.push(color["value"]);
+        });
+      }
+      getData();
+
+      var ctx = document.getElementById(id);
+      var myChart = new Chart(ctx, {
+        type: type,
+        data: {
+          labels: arrayData1,
+          datasets: [
+            {
+              label: label,
+              data: arrayData2,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            xAxes: [{ display: verX }],
+            yAxes: [
+              {
+                display: verY,
+                ticks: {
+                  beginAtZero: beginAtZero,
+                },
+              },
+            ],
+          },
+          legend: {
+            display: legend,
+          },
+        },
+      });
+      //console.log(letter["color"]);
+    }, 500);
+  }
+}
+export { loadOn, dinamicCss, chartJs };
